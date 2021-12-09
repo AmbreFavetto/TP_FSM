b8214b62440e75b900c0f75b5a888323400ee992
valentin Houdu <valentin.hpro1@gmail.com>
2021-12-09 16:35:56 +0100

    lol

--- uiSearchFiles/mainwindow.cpp
+++ uiSearchFiles/mainwindow.cpp
@@ -1,18 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QMainWindow>
-#include "myclient.h"
 
-QT_BEGIN_NAMESPACE
-namespace Ui {
-    class MainWindow;
-}
-QT_END_NAMESPACE
-
-class MinWIndow : public QMainWindow {
-    Q_OBJECT
-    myclient *_client = nullptr;
-};
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
