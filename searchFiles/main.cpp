72622402b1021a9d90d029ae195001ea5fa0eb14
valentin Houdu <valentin.hpro1@gmail.com>
2022-01-14 11:16:53 +0100

    J'ai une grosse bite, cheh alexis part2

--- searchFiles/main.cpp
+++ searchFiles/main.cpp
@@ -11,25 +11,23 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    //MainWindow w;
-    //w.show();
+    MainWindow w;
+    w.show();
+
+
     /* Debut timer */
-    QElapsedTimer timer;
-    timer.start();
+    //QElapsedTimer timer;
+    //timer.start();
 
-    bddRequest * db = new bddRequest();
-
-    db->setupDatabase();
-    QString location = "C:/";
-
+    //bddRequest * db = new bddRequest();
+    //db->setupDatabase();
+    //QString location = "C:/";
     //QString location = "C:/Users/Leval/TPDevLogicielAvance2";
-    db->directoryIterator(location);
+    //db->directoryIterator(location);
+    //qDebug() << timer.elapsed()/1000 << "seconde";
 
-
-    qDebug() << timer.elapsed()/1000 << "seconde";
-
-    return 0;
-    //return a.exec();
+    //return 0;
+    return a.exec();
 }
 
 
